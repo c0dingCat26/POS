cf096240f9ab940e2deb67843c00f383497bd9c7
Abdul Saqib <saquibsqb31@gmail.com>
2021-05-19 18:44:12 +0530

    add notification settings page

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 	http.HandleFunc("/addquotation", addquotation)
 	http.HandleFunc("/modifyquotation", modifyquotation)
 	http.HandleFunc("/settings/general", settings)
+	http.HandleFunc("/settings/notification", notificationSettings)
 	http.ListenAndServe(":8085", nil)
 }
 
@@ -221,6 +222,15 @@ func modifyquotation(w http.ResponseWriter, r *http.Request) {
 }
 
 func settings(w http.ResponseWriter, r *http.Request) {
+	files := append(layoutFiles(), "./mock/settings.html")
+	t := template.Must(template.ParseFiles(files...))
+	err := t.ExecuteTemplate(w, "bootstrap", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func notificationSettings(w http.ResponseWriter, r *http.Request) {
 	files := append(layoutFiles(), "./mock/settings.html")
 	t := template.Must(template.ParseFiles(files...))
 	err := t.ExecuteTemplate(w, "bootstrap", nil)
